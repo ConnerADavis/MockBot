9690d55a437cb3fc09ff667f6ed71deb7c347a9b
Conner Davis <conner.davis.17@cnu.edu>
2018-04-03 23:10:24 -0400

    working except images

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+import random
 
 TOKEN = "NDMwNzQ4Nzg2NDIwMDg4ODQy.DaUt4Q.lPd71aYiHRfjNY-cmEmNLNeUPUw"
 
@@ -10,8 +11,26 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("!hello"):
-        msg = 'Hello {0.author.mention}'.format(message)
+    if message.content.startswith("!mock"):
+        user = message.mentions[0]
+        found = False
+        async for m in client.logs_from(message.channel):
+            if m.author == user:
+                lastmessage = m.content
+                found = True
+                break
+        msg = "No recent message by that user".format(message)
+        if found and lastmessage != "":
+            msg = ""
+            characters = list(lastmessage)
+            for c in characters:
+                tmp = c
+                if c.isalpha():
+                    if random.randint(0, 10) < 5:
+                        tmp = tmp.upper()
+                    else:
+                        tmp = tmp.lower()
+                msg += tmp
         await client.send_message(message.channel, msg)
 
 @client.event
